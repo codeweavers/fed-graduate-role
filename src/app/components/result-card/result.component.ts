3eb849e4559aaaba5f44e421b632bc65a2ad7c15
Victoria Lampard <vglampard@protonmail.com>
2023-04-09 16:59:42 +0100

    streamlined card styling for main display

--- src/app/components/result-card/result.component.ts
+++ src/app/components/result-card/result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PokemonType } from 'PokemonType';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-result',
@@ -10,55 +11,13 @@ export class ResultComponent implements OnInit {
   @Input() pokemon: PokemonType;
   types: any;
 
-  constructor() {}
-  // Function to get correct colour from type that can be passed into the conditional styling. Initially I tried using ternary chaining here, but it wasn't working beyond two colours - and It's generally bad practice as I understand (although I'm not convinced this is easier to read)
-  getTypeColour(type: string): string {
-    switch (type) {
-      case 'psychic':
-        return '#F85888';
-      case 'electric':
-        return '#F8D030';
-      case 'grass':
-        return '#78C850';
-      case 'fire':
-        return '#F08030';
-      case 'water':
-        return '#6890F0';
-      case 'normal':
-        return '#A8A878';
-      case 'rock':
-        return '#B8A038';
-      case 'fighting':
-        return '#C03028';
-      case 'ice':
-        return '#98D8D8';
-      case 'bug':
-        return '#A8B820';
-      case 'flying':
-        return '#A890F0';
-      case 'dragon':
-        return '#7038F8';
-      case 'ghost':
-        return '#705898';
-      case 'poison':
-        return '#A040A0';
-      case 'dark':
-        return '#705848';
-      case 'steel':
-        return '#B8B8D0';
-      case 'ground':
-        return '#E0C068';
-      case 'fairy':
-        return '#ffa8c2';
-      default:
-        return 'black';
-    }
-  }
+  constructor( private pokemonService: PokemonService) {}
+  
 
   ngOnInit(): void {
     // Create an array of objects to use for conditional rendering, with each object containng the type name and the colour code associated with it
     this.types = this.pokemon.types.map((type) => {
-      return {name: type.type.name, colour: this.getTypeColour(type.type.name) }
+      return {name: type.type.name, colour: this.pokemonService.getTypeColour(type.type.name) }
     });
   }
 }
--- src/app/components/result-detail/result-detail.component.ts
+++ src/app/components/result-detail/result-detail.component.ts
@@ -8,7 +8,6 @@ import { PokemonType } from 'PokemonType';
   templateUrl: './result-detail.component.html',
   styleUrls: ['./result-detail.component.scss'],
 })
-
 export class ResultDetailComponent implements OnInit {
   pokemonName: any;
   pokemon: Object;
@@ -20,63 +19,25 @@ export class ResultDetailComponent implements OnInit {
     private _Activatedroute: ActivatedRoute
   ) {}
 
-  getAvailableSprites(sprites: object){
-    let spritesArray = Object.entries(sprites)
-    this.defaultSprites = spritesArray.filter(sprite=> sprite[0].includes("default"))   
-    }
+  getAvailableSprites(sprites: object) {
+    let spritesArray = Object.entries(sprites);
+    this.defaultSprites = spritesArray.filter((sprite) =>
+      sprite[0].includes('default')
+    );
+  }
 
-    // Function to get correct colour from type that can be passed into the conditional styling. Initially I tried using ternary chaining here, but it wasn't working beyond two colours - and It's generally bad practice as I understand (although I'm not convinced this is easier to read)
-  getTypeColour(type: string): string {
-    switch (type) {
-      case 'psychic':
-        return '#F85888';
-      case 'electric':
-        return '#F8D030';
-      case 'grass':
-        return '#78C850';
-      case 'fire':
-        return '#F08030';
-      case 'water':
-        return '#6890F0';
-      case 'normal':
-        return '#A8A878';
-      case 'rock':
-        return '#B8A038';
-      case 'fighting':
-        return '#C03028';
-      case 'ice':
-        return '#98D8D8';
-      case 'bug':
-        return '#A8B820';
-      case 'flying':
-        return '#A890F0';
-      case 'dragon':
-        return '#7038F8';
-      case 'ghost':
-        return '#705898';
-      case 'poison':
-        return '#A040A0';
-      case 'dark':
-        return '#705848';
-      case 'steel':
-        return '#B8B8D0';
-      case 'ground':
-        return '#E0C068';
-      case 'fairy':
-        return '#ffa8c2';
-      default:
-        return 'black';
-    }
+  getCardColour(pokemon: PokemonType): string {
+    return this.pokemonService.getTypeColour(pokemon.types[0].type.name);
   }
   ngOnInit(): void {
     this.pokemonName = this._Activatedroute.snapshot.paramMap.get('name');
     //PUT IN ERROR HANDLING!
-      this.pokemonService
-        .getSpecificPokemon(this.pokemonName)
-        .subscribe((pokemon) => {
-          console.log("POKEMON:", pokemon);
-          this.getAvailableSprites(pokemon.sprites);
-          this.pokemonData = pokemon
-        })
+    this.pokemonService
+      .getSpecificPokemon(this.pokemonName)
+      .subscribe((pokemon) => {
+        console.log('POKEMON:', pokemon);
+        this.getAvailableSprites(pokemon.sprites);
+        this.pokemonData = pokemon;
+      });
   }
 }
