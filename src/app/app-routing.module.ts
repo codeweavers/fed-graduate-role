1188e6f65425f3d3fba1b1d771829189d1fd1683
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2023-04-12 16:25:15 +0100

    ran angular set up test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'hello', component: HelloWorldComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
